97006d4e6311496dc6bdb6043e3d3dbacdd49b1b
primaam <pmaharyono@gmail.com>
2023-07-28 11:03:14 +0700

    update

--- research-todo-react/src/App.js
+++ research-todo-react/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoRowItem from './components/TodoRowItem';
+import React from 'react';
 
 function App() {
+  const [todosState, setTodosState] = React.useState([])
+
+  const addTodo = ()=>{
+    if(todosState.length > 0){
+      const newTodo = {
+        rowNumber: todosState.length+1,
+        rowDescription: `description ${todosState.length + 1}`,
+        rowAssigned: `user ${todosState.length +1}`
+      }
+      setTodosState([...todosState, newTodo])
+     
+    }else{
+      const newTodo = {
+        rowNumber: 1,
+        rowDescription: `description 1`,
+        rowAssigned: `user 1`
+      }
+      setTodosState([newTodo])
+      
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className='card'>
+        <div className='card-header' style={{flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between',display: 'flex', flex:1}}>
+          <div>
+            Your Todo's
+          </div>
+          <button className='btn btn-primary' onClick={addTodo}>
+            Add New Todo
+          </button>
+        </div>
+        <div className='card-body'>
+          <table className='table table-hover'>
+            <thead>
+              <tr>
+                <th scope='col'>#</th>
+                <th scope='col'>Decription</th>
+                <th scope='col'>Assigned</th>
+              </tr>
+            </thead>
+            <tbody>
+              {todosState?.map((item, i)=>{
+                return(
+                  <TodoRowItem
+                    key={i}
+                    rowDescription={item.rowDescription}
+                    rowNumber={item.rowNumber}
+                    rowAssigned={item.rowAssigned}
+                  />
+                )
+              })}
+            </tbody>
+          </table>
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ research-todo-react/src/components/TodoRowItem.js
@@ -0,0 +1,21 @@
+
+
+const TodoRowItem = (props)=>{
+
+    const {
+        rowNumber,
+        rowDescription,
+        rowAssigned
+    } = props
+
+    console.log("test re-render", rowNumber, rowDescription, rowAssigned)
+    return (
+        <tr>
+            <th scope='row'>{rowNumber}</th>
+            <td>{rowDescription}</td>
+            <td>{rowAssigned}</td>
+        </tr>
+    )
+}
+
+export default TodoRowItem
