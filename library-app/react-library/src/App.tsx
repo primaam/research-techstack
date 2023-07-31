ddf1620947efd921b5334e817f079ea5c1f6c482
primaam <pmaharyono@gmail.com>
2023-07-31 15:55:52 +0700

    add carousel & header component

--- library-app/react-library/src/App.tsx
+++ library-app/react-library/src/App.tsx
@@ -2,9 +2,17 @@ import React from "react";
 
 import "./App.css";
 import Navbar from "./layouts/Navbar";
+import ExploreTopBooks from "./layouts/ExploreTopBooks";
+import Carousel from "./layouts/Carousel";
 
 function App() {
-  return <Navbar />;
+  return (
+    <React.Fragment>
+      <Navbar />
+      <ExploreTopBooks />
+      <Carousel />
+    </React.Fragment>
+  );
 }
 
 export default App;
